64523182dcc06a881538991bee8b349068496988
afrivera <andresrfelip@hotmail.com>
2022-06-14 20:24:57 -0500

    config shared components with its routes

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+  `
   ]
 })
 export class MenuComponent implements OnInit {
