dcbf8957b404f4b14135c328fe3941cfba05558d
afrivera <andresrfelip@hotmail.com>
2022-06-17 21:44:50 -0500

    end session 21 config properties, minimap

--- src/app/maps/components/min-map/min-map.component.ts
+++ src/app/maps/components/min-map/min-map.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-min-map',
   templateUrl: './min-map.component.html',
-  styles: [
+  styles: [`
+    div {
+      width: 100%;
+      height: 150px;
+      margin: 0px;
+    }
+  `
   ]
 })
-export class MinMapComponent implements OnInit {
+export class MinMapComponent implements AfterViewInit {
 
+  @Input() lngLat: [ number, number ] = [ 0,0 ];
+  @ViewChild('map') divMap !: ElementRef;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    })
+
+    new mapboxgl.Marker()
+      .setLngLat( this.lngLat )
+      .addTo( map )
   }
 
 }
